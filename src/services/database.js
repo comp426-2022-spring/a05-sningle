4f81f3167cc5b0b0f035eb0b12cfb7f8ae64aecf
Savannah Ingle <sningle@email.unc.edu>
2022-04-23 21:20:08 -0400

    added endpoints and tried to start hooking up html

--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,34 @@
 // Put your database code here
+const database = require('better-sqlite3')
+const Database = require('better-sqlite3/lib/database')
+const { append } = require('express/lib/response')
+const db = new Database('log.db')
+
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+let row = stmt.get();
+
+
+if (row === undefined) {
+    console.log('Log database appears to be empty. Creating log database...')
+
+    const sqlInit = `
+        CREATE TABLE accesslog ( 
+            id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT, 
+            remoteuser TEXT, 
+            time TEXT, 
+            method TEXT, 
+            url TEXT, 
+            protocol TEXT,
+            httpversion TEXT, 
+            status TEXT, 
+            referer TEXT, 
+            useragent TEXT )
+    `
+
+    db.exec(sqlInit)
+} else {
+    console.log('Log database exists.')
+}
+
+module.exports = db
